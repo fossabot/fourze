28e5b50255548e8d5b88de64cbe1155acb2ba8a6
chizuki <kritsu@qq.com>
2022-07-22 13:49:27 +0800

    chore:change to unbuild.

---
+++ playgrounds/server/main.ts
@@ -0,0 +1,29 @@
+import comporession from "compression"
+
+import { CommonMiddleware, createApp, createRenderer, createRouter } from "@fourze/server"
+import path from "path"
+
+const router = createRouter(route => {
+    route("GET:/hello", () => {
+        return {
+            msg: "hello router 1"
+        }
+    })
+})
+
+const router2 = createRouter(route => {
+    route("GET:/test/hello", () => {
+        return {
+            msg: "hello router 2"
+        }
+    })
+})
+
+const renderer = createRenderer({ dir: path.resolve(process.cwd(), "../web/dist"), fallbacks: { "/home": "/" } })
+
+const app = createApp()
+
+app.use("/test", comporession({ threshold: 0 }) as CommonMiddleware)
+app.use(router, router2)
+app.use(renderer)
+app.listen(3000)
