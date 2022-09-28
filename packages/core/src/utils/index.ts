bf668a9a2f788fbf9e26cc74734783f9ddb50059
chizuki <kritsu@qq.com>
2022-09-28 15:54:47 +0800

    new:add utils.

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -1,17 +1,4 @@
-import { Pagination, PagingData } from "../types"
-
-export function slicePage<T>(source: T[], pagination: Pagination): PagingData<T> {
-    const { page = 1, size = 10 } = pagination
-    const total = source.length
-    const data = source.slice((page - 1) * size, page * size)
-    return {
-        total,
-        page,
-        size,
-        data
-    }
-}
-
+export * from "./common"
 export * from "./faker"
 export * from "./overload"
 export * from "./promise"
