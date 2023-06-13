1502b6e27960d4e7d5bb715bbfda513ffee2c6d1
chizukicn <chizukicn@outlook.com>
2023-06-13 23:26:00 +0800

    chore(test): remove resolve-host-name test.

--- packages/server/test/utils.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "vitest";
-import { resolveHostname } from "../src/utils";
-
-test("test-resolve-hostname", async () => {
-  const host = await resolveHostname("localhost");
-  expect(host.name).toEqual("127.0.0.1");
-});
